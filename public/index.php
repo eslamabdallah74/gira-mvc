36d10d926db1493142c65acb53c8fad4d5c82ccc
eslamabdallah74 <eslamabdallah301@gmail.com>
2023-03-14 07:25:07 +0200

    making base controller

--- public/index.php
+++ public/index.php
@@ -2,13 +2,14 @@
 require_once __DIR__ . '/../vendor/autoload.php';
 //* Start Gira
 
+use gira\controllers\HomeController;
 use gira\controllers\loginController;
 use gira\core\Gira;
 
 
 $app = new Gira(dirname(__DIR__));
-$app->router->get('/', 'home');
-$app->router->get('/users', 'users');
+$app->router->get('/', [HomeController::class,'index']);
+$app->router->get('/users', [loginController::class, 'index']);
 $app->router->post('/users', [loginController::class, 'login']);
 
 
