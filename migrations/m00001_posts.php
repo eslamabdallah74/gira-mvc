6121b8e20ac8a56c7c3ab63d234573a27d7f110e
eslamabdallah74 <eslamabdallah301@gmail.com>
2023-03-19 06:52:27 +0200

    migration system done

--- migrations/m00001_posts.php
+++ migrations/m00001_posts.php
@@ -1,15 +1,25 @@
 <?php
 
+use gira\core\Gira;
 
 class m00001_posts
 {
   public function up()
   {
-    // Add migration logic here
+    $db  = Gira::$app->database;
+    $SQL = "CREATE TABLE posts (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      body VARCHAR(255) NOT NULL,
+      slug varchar(255),
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=INNODB;";
+    $db->pdo->exec($SQL);
   }
 
   public function down()
   {
-    // Add rollback logic here
+    $db  = Gira::$app->database;
+    $SQL = "DROP TABLE posts;";
+    $db->pdo->exec($SQL);
   }
 }
--- migrations/m00002_users.php
+++ migrations/m00002_users.php
@@ -1,15 +1,26 @@
 <?php
 
+use gira\core\Gira;
 
 class m00002_users
 {
   public function up()
   {
-    // Add migration logic here
+    $db  = Gira::$app->database;
+    $SQL = "CREATE TABLE users (
+      id INT AUTO_INCREMENT PRIMARY KEY,
+      name VARCHAR(255) NOT NULL,
+      email VARCHAR(255) NOT NULL UNIQUE,
+      password VARCHAR(255) NOT NULL,
+      created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    ) ENGINE=INNODB;";
+    $db->pdo->exec($SQL);
   }
 
   public function down()
   {
-    // Add rollback logic here
+    $db  = Gira::$app->database;
+    $SQL = "DROP TABLE users;";
+    $db->pdo->exec($SQL);  
   }
 }
